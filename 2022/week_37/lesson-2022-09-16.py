4ace61949475d75d28e9ac8aa0f70e48a2b048d0
Lasse Wolpmann <lassewolpmann@outlook.de>
2022-09-16 11:15:49 +0300

    added lesson-2022-09-16.py

--- 2022/week_37/lesson-2022-09-16.py
+++ 2022/week_37/lesson-2022-09-16.py
@@ -23,3 +23,49 @@ print(s.find(' g'))     # Finding the index for the first " g", WITH the whitesp
 # TIME MODULE
 import time
 now = time.ctime()
+current_date = now.split()
+print(current_date)
+current_time = current_date[3]
+print(current_time)
+hms = current_time.split(':')   # hms = hours minutes seconds
+print(f'{hms[0]} hours {hms[1]} minutes and {hms[2]} seconds')
+
+
+# BACK TO STRING METHODS
+sentence = """Hello
+Python
+again
+"""
+print(sentence)
+sentence_list = sentence.splitlines()   # Splitting the string at each newline
+print(sentence_list[::-1])  # ::-1 to reverse list
+
+s2 = 'great britain'
+print(s2.title())   # Turns sentence into titlecase
+print(s2.count('e'))    # Counts the amount of "e"
+
+number_string = '753'
+print(number_string.isalnum())  # Checks if string contains only alphanumerics (Alphabet and numbers)
+letter_string = 'abc'
+print(letter_string.isalpha())  # Check if string contains only alphabet
+
+Bond = '7'.zfill(3)     # Fills string in the beginning with zeroes until given "width" is achieved
+print(Bond)
+
+whitespace_string = '       Test string with a lot of whitespaces      '
+print(whitespace_string.strip())    # Removes all leading and trailing whitespaces
+
+ans = input('Your ID: ').strip()    # Remove accidental whitespaces for "clean" input
+print(ans)
+
+
+# TRUE AND FALSE
+# True can be expressed as number 1, and number 1 as True
+print(type(True))
+print(int(True))
+print(bool(1))
+
+# False can be expressed as number 0, and number 0 as False
+print(type(False))
+print(int(False))
+print(bool(0))
