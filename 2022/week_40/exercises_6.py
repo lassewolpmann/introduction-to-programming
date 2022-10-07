0291c5a51eae951e6c9106992e7c165b5392727b
Lasse Wolpmann <lassewolpmann@outlook.de>
2022-10-07 12:08:20 +0300

    added exercises_6.py

--- 2022/week_40/exercises_6.py
+++ 2022/week_40/exercises_6.py
@@ -17,7 +17,7 @@ def task_1():
     # timeit.timeit("2 ** 10")
     # 0.010506924998480827 seconds
     import timeit
-    print(f'Calculating 2 ** 100 takes {timeit.timeit("2 ** 100")} seconds')
+    print(f'Calculating 2 ** 10 takes {timeit.timeit("2 ** 10")} seconds')
 
     # Check the running times creating the literal list like list_ and the running time of the literal like tuple_
     # How do you compare list and tuple?
@@ -31,14 +31,15 @@ def task_1():
     # c) Manipulating with tuples
     # name, age, country
     item1 = ('Charlie Brown', 11, 'USA')
-    name = item1[0]
-    age = item1[1]
-    country = item1[2]
-    print(f'Name is {name}, age is {age}, country is {country}')
+    # name = item1[0]
+    # age = item1[1]
+    # country = item1[2]
+    # print(f'Name is {name}, age is {age}, country is {country}')
     # Name is Charlie Brown, age is 11, country is USA
-    # How to derive name, age and country with unpacking of tuples?
 
-    # By accessing the indexes of the tuple
+    # How to derive name, age and country with unpacking of tuples?
+    (name, age, country) = item1
+    print(f'Name is {name}, age is {age}, country is {country}')
 
 
 def task_2():
@@ -52,7 +53,7 @@ def task_2():
     print(li)
 
     # b) Add the name ‘Charlie’ between ‘Liza’ and ‘Peter’ using an appropriate method.
-    li.insert(2, 'Charlie')
+    li.insert(li.index('Peter'), 'Charlie')
     print(li)
 
     # c) String has a method join(). Explain, how to use it.
@@ -72,46 +73,68 @@ def task_2():
 
 
 def task_3():
-    pass
     # 3
     # This exercise uses functions of random module and ranges and lists.
     # list(range(1, 4)) returns [1, 2, 3]
+
     # a) Your program prints [2, 4, 6, 8, 9, 7, 5, 3, 1] using range and + operator.
+    print(list(range(2, 9, 2)) + list(range(9, 0, -2)))
 
     # b) The code
-    # import random
-    # li = ['John', 'Liza', 'Peter', 'Mary’]
-    # should give a random word.
+    import random
+    li = ['John', 'Liza', 'Peter', 'Mary']
+    # should give a random word
     # Hint: Check the random module function choice()
-    #
-    # c) We roll a dice 12 times.
-    # rolls = random.choices(range(1, 7), k = 12)
+    print(random.choice(li))
+
+    # c) We roll a die 12 times
+    rolls: list = random.choices(range(1, 7), k=12)
+    print(rolls)
     # How many 6’s are there?
+    print(f"The list contains {rolls.count(6)} 6's")
     # Hint: Find the correct list method.
-    #
+
     # d) The Finnish lottery takes 7 random integers between 1-40 (both included).
-    # fin_lot = random.sample(range(1, 41), 7)
+    fin_lot: list = random.sample(range(1, 41), 7)
+
     # Sort the numbers from the smallest to the greatest.
+    fin_lot.sort()
+    print(fin_lot)
 
 
 def task_4():
-    pass
     # 4
     # We have a deck of playing cards.
-    # suits = ['♣', '♦', '♥', '♠']
-    # values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    # deck = [(s, v) for s in suits for v in values]
+    suits = ['♣', '♦', '♥', '♠']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    deck = [(s, v) for s in suits for v in values]
+    print(deck)
     # a)
     # Calculate the number of the cards of the deck.
-    #
-    # b) Change the suit of the second card ('♣', '3') to the card (‘♠’, '3').What are your notions and why?
-    #
+    print(len(deck))
+
+    # b) Change the suit of the second card ('♣', '3') to the card (‘♠’, '3').
+    # What are your notions and why?
+    deck[1] = (suits[3], values[1])
+    print(deck)
+
     # c) Shuffle the deck
     # Take one random card and print it in the form e.g. ♣K
-    #
+    import random
+
+    random_card = random.choice(deck)
+    print(f'{random_card[0]}{random_card[1]}')
+
     # d) Deal the cards for 4 players.
+    for player in range(4):
+        r_card = random.choice(deck)
+        deck.pop(deck.index(r_card))
+
+        print(f'Card for Player {player + 1} is {r_card}')
 
 
 if __name__ == '__main__':
     task_1()
     task_2()
+    task_3()
+    task_4()
