83f8daa58d0fbad8e14174b542d31c62bd64033c
Lasse Wolpmann <lassewolpmann@outlook.de>
2022-11-29 09:06:22 +0200

    added solutions to exercises_11.py

---
+++ 2022/week_46/exercises_11.py
@@ -0,0 +1,61 @@
+def task_1():
+    # Read the text file and print all unique words
+    with open('goodMorning.txt') as file:
+        text = file.read()
+
+        for word in text.split():
+            if word.isalpha():
+                print(word)
+
+
+def task_2():
+    # Read the text file line by line using readline().
+    # Print only the lines that contain at least one number character.
+    import string
+    with open('goodMorning.txt') as file:
+        while True:
+            line = file.readline()
+
+            # Check if line is not empty
+            if line:
+                for n in string.digits:
+                    # If number is in line, print line out and break loop to go to next line
+                    if n in line.split():
+                        print(line)
+                        break
+
+            else:
+                break
+
+
+def task_3(filename):
+    with open(filename, 'w') as file:
+        while True:
+            word = input('Give word (write "end" to stop program): ')
+
+            if word == 'end':
+                break
+
+            else:
+                file.write(f'{word}\n')
+
+
+def task_4():
+    with open('dictionary.txt') as file:
+        dictionary = file.readlines()
+        word_dict = {line.split()[0]: line.split()[1] for line in dictionary}
+
+        correct_guesses = 0
+        for finnish_word in word_dict:
+            english_word = word_dict.get(finnish_word)
+            if input(f'What is the english word for {finnish_word}: ') == english_word:
+                correct_guesses += 1
+
+        print(f'You guessed {correct_guesses} translations correct!')
+
+
+if __name__ == '__main__':
+    task_1()
+    task_2()
+    task_3('test.txt')
+    task_4()
